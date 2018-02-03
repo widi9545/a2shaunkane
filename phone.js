25c1677083f69bdf22200a0089b56e6f12eba1d6
widi9545 <widi9545@colorado.edu>
2018-02-03 15:49:50 -0700

    Add files via upload

--- phone.js
+++ phone.js
@@ -5,35 +5,35 @@ $(document).ready(function() { // do this when the document is loaded
 	$("#addcontact").show(); // show the element with ID "element"
 	$("#dialer").hide(); // hide the element with ID "otherElement"
 	$("#contactlist").hide();
-	$("gesturebox").hide();
+	$("#gesturebox").hide();
 });
 
 $("#dialerbutton").click(function() { // when "button_id" is clicked
 	$("#dialer").show(); // show element
 	$("#contactlist").hide();	// hide other element
 	$("#addcontact").hide();
-	$("gesturebox").hide();
+	$("#gesturebox").hide();
 });
 
 $("#contactlistbutton").click(function() { // when "button_id" is clicked
 	$("#contactlist").show(); // show element
 	$("#dialer").hide();	// hide other element
 	$("#addcontact").hide();
-	$("gesturebox").hide();
+	$("#gesturebox").hide();
 });
 
 $("#addcontactbutton").click(function() { // when "button_id" is clicked
 	$("#addcontact").show(); // show element
 	$("#dialer").hide();	// hide other element
 	$("#contactlist").hide();
-	$("gesturebox").hide();
+	$("#gesturebox").hide();
 });
 
 $("#gestureareabutton").click(function() { // when "button_id" is clicked
 	$("#gesturebox").show(); // show element
 	$("#dialer").hide();	// hide other element
 	$("#contactlist").hide();
-	$("addcontact").hide();
+	$("#addcontact").hide();
 });
 
 
