3c18504460b947fbbcec1975147f9d467a155758
widi9545 <widi9545@colorado.edu>
2018-02-03 18:11:30 -0700

    Add files via upload

--- phone.js
+++ phone.js
@@ -89,13 +89,19 @@ $("#dialerclear").click(function() { // when "button_id" is clicked
 
 $("#gesturearea").mousedown(function(event) {
 	$("#gesture_output").val("Down")
+	mouseX = event.pageX
+	mouseY = event.pageY
+	alert(mouseX)
+	alert(mouseY)
 	
 
 	$("#gesturearea").mousemove(function(event) {
 		var msg = "Handler for mousemove called at";
 		msg += event.pageX + ", " + event.pageY;
-		$("#log").append("<div>" + msg + "</div>");
-		$(alert(msg))
+
+
+		//$("#log").append("<div>" + msg + "</div>");
+		//$(alert(msg))
 	});
 
 	//$(alert("hello"))
