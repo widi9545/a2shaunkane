548d1f6d6b6d1312f7fef0bf2019a270581d04b6
widi9545 <widi9545@colorado.edu>
2018-02-03 17:30:33 -0700

    Add files via upload

--- phone.js
+++ phone.js
@@ -88,7 +88,7 @@ $("#dialerclear").click(function() { // when "button_id" is clicked
 
 $("#gesturearea").mousedown(function() {
 	$("#gesture_output").val("Down")
-	alert("hello");
+	$(alert("hello") var x = "0")
 });
 
 $("#gesturearea").mouseup(function() {
